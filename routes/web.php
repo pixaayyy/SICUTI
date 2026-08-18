cc4e612fc220554bcbe7b53fb5506c4dfa73996c
nyomandinda45 <nyomandindasafitri.stu@pnc.ac.id>
2026-08-18 11:00:43 +0700

    update di edit.blade, profilecontroller dan routes

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,7 @@ Route::get('/dashboard', function () {
 ->middleware(['auth', 'verified'])
 ->name('dashboard');
 
+// Route Karyawan
 Route::middleware(['auth'])
     ->prefix('karyawan')
     ->name('karyawan.')
@@ -27,9 +28,26 @@ Route::middleware(['auth'])
         Route::get('/profil', [ProfileController::class, 'edit'])->name('profil');
     });
 
+// Route Profile Default
 Route::middleware(['auth'])->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::middleware(['auth'])
+    ->prefix('karyawan')
+    ->name('karyawan.')
+    ->group(function () {
+        Route::get('/dashboard', [DashboardkController::class, 'index'])->name('dashboard');
+        Route::get('/ajukan-cuti', [CutiController::class, 'create'])->name('cuti.create');
+        Route::post('/ajukan-cuti', [CutiController::class, 'store'])->name('cuti.store');
+        Route::get('/status-pengajuan', [CutiController::class, 'status'])->name('cuti.status');
+        Route::get('/riwayat-cuti', [CutiController::class, 'index'])->name('cuti.index');
+        Route::get('/profil', [ProfileController::class, 'edit'])->name('profil');
+        
+        // TAMBAHKAN BARIS INI AGAR SIDEBAR TETAP AKTIF DAN REDIRECT PAS
+        Route::patch('/profil', [ProfileController::class, 'update'])->name('profil.update');
+    });
 });
 
+// TAMBAHKAN BARIS INI: Untuk memanggil semua route autentikasi bawaan Laravel (termasuk password.update)
+require __DIR__.'/auth.php';
