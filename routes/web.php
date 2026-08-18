620d8206b091a8a9443636c9f8c115eb6f4350c2
nyomandinda45 <nyomandindasafitri.stu@pnc.ac.id>
2026-08-18 11:17:34 +0700

    merapihkan web.php

--- routes/web.php
+++ routes/web.php
@@ -11,15 +11,10 @@ Route::get('/', function () {
 
 Route::get('/dashboard', function () {
     return view('dashboard');
-})
-->middleware(['auth', 'verified'])
-->name('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
 // Route Karyawan
-Route::middleware(['auth'])
-    ->prefix('karyawan')
-    ->name('karyawan.')
-    ->group(function () {
+Route::middleware(['auth'])->prefix('karyawan')->name('karyawan.')->group(function () {
         Route::get('/dashboard', [DashboardkController::class, 'index'])->name('dashboard');
         Route::get('/ajukan-cuti', [CutiController::class, 'create'])->name('cuti.create');
         Route::post('/ajukan-cuti', [CutiController::class, 'store'])->name('cuti.store');
@@ -33,18 +28,13 @@ Route::middleware(['auth'])->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    Route::middleware(['auth'])
-    ->prefix('karyawan')
-    ->name('karyawan.')
-    ->group(function () {
+    Route::middleware(['auth'])->prefix('karyawan')->name('karyawan.')->group(function () {
         Route::get('/dashboard', [DashboardkController::class, 'index'])->name('dashboard');
         Route::get('/ajukan-cuti', [CutiController::class, 'create'])->name('cuti.create');
         Route::post('/ajukan-cuti', [CutiController::class, 'store'])->name('cuti.store');
         Route::get('/status-pengajuan', [CutiController::class, 'status'])->name('cuti.status');
         Route::get('/riwayat-cuti', [CutiController::class, 'index'])->name('cuti.index');
-        Route::get('/profil', [ProfileController::class, 'edit'])->name('profil');
-        
-        // TAMBAHKAN BARIS INI AGAR SIDEBAR TETAP AKTIF DAN REDIRECT PAS
+        Route::get('/profil', [ProfileController::class, 'edit'])->name('profil');        
         Route::patch('/profil', [ProfileController::class, 'update'])->name('profil.update');
     });
 });
